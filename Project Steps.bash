3b9594c9fa85a498268b5c4eaaeac762839a5661
Ahmed Ehsan <95180897+ahmedehsaan@users.noreply.github.com>
2023-05-05 09:29:54 +0300

    Add files via upload

---
+++ Project Steps.bash
@@ -0,0 +1,8 @@
+#-------> create folder MySQL at /home and give it all file permissions.
+#mkdir /home/MySQL
+#chmod 777 /home/MySQL
+#------->There will be a main script called MySQL/main.sh.
+#touch /home/MySQL/main.sh
+#------->There will be in a file called DB_admins.db will have a list of admin users, including oracle and other created users.
+#touch /home/MySQL/DB_admins.db
+
